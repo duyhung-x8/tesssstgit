66263ce86ca011a1baff67fa8a4353236ed6b458
Duy Hung <hungnd@rikkeisoft.com>
2014-06-12 16:36:37 -0700

    commint ImageLoader

--- app/src/main/java/com/example/myapplication3/app/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/app/MainActivity.java
@@ -4,20 +4,41 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    ImageView img;
+
+    @Override
+    public void supportInvalidateOptionsMenu() {
+        super.supportInvalidateOptionsMenu();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img = (ImageView) findViewById(R.id.img1);
+
+        String url = "http://api.androidhive.info/images/sample.jpg";
+        String url3 ="drawable://" + R.drawable.ic_launcher;
+
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext()).build();
+        imageLoader.init(config);
+        imageLoader.displayImage(url3,img);
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
